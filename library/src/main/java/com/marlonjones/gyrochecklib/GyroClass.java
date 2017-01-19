ca48a3a0b5f42a23843d74963972a29d879d8d20
Marlon Jones <marlonjones234@gmail.com>
2017-01-19 11:12:56 -0600

    System Event Listener

--- library/src/main/java/com/marlonjones/gyrochecklib/GyroClass.java
+++ library/src/main/java/com/marlonjones/gyrochecklib/GyroClass.java
@@ -1,9 +1,14 @@
 package com.marlonjones.gyrochecklib;
 
+import android.content.Context;
+import android.hardware.SensorEventListener;
+
 /**
  * Created by Marlon Jones (@MJonesDev on 1/19/2017.
  github.com/MJonesDev
  */
 
-public class GyroClass {
+public class GyroClass implements SensorEventListener {
+    private Context context;
+
 }
