c05a15a8fb7e2e70b7d192dcda120d7c69da82a3
Marlon Jones <marlonjones234@gmail.com>
2017-01-19 11:29:36 -0600

    Full class

--- library/src/main/java/com/marlonjones/gyrochecklib/GyroClass.java
+++ library/src/main/java/com/marlonjones/gyrochecklib/GyroClass.java
@@ -2,6 +2,7 @@ package com.marlonjones.gyrochecklib;
 
 import android.content.Context;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.widget.LinearLayout;
@@ -29,6 +30,22 @@ public abstract class GyroClass extends LinearLayout implements SensorEventListe
         textX = (TextView) findViewById(R.id.TextX);
         textY = (TextView) findViewById(R.id.TextY);
         textZ = (TextView) findViewById(R.id.TextZ);
+        sensorManager.registerListener(gyroListener, sensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+
+    public SensorEventListener gyroListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int acc) { }
+
+        public void onSensorChanged(SensorEvent event) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+
+            textX.setText("X : " + (int)x + " rad/s");
+            textY.setText("Y : " + (int)y + " rad/s");
+            textZ.setText("Z : " + (int)z + " rad/s");
+        }
+    };
 }
