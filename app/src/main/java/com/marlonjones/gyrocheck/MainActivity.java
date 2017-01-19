608d6b854f2dc7d7504d5cc5d6707222d085d386
Marlon Jones <marlonjones234@gmail.com>
2017-01-19 13:11:42 -0600

    test code

--- app/src/main/java/com/marlonjones/gyrocheck/MainActivity.java
+++ app/src/main/java/com/marlonjones/gyrocheck/MainActivity.java
@@ -1,15 +1,12 @@
 package com.marlonjones.gyrocheck;
 
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
+import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+
 
 import com.marlonjones.gyrochecklib.GyroClass;
 
@@ -21,8 +18,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        GyroClass gyro = new GyroClass(getApplicationContext());
-        setContentView(gyro);
-
+        View v = new GyroClass (getApplicationContext());
+        setContentView(v);
     }
 }
