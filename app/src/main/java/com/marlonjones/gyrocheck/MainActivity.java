73c47f18edc70cfcba166b56dbfc9ec2ba1a6467
Marlon Jones <marlonjones234@gmail.com>
2017-02-16 09:41:07 -0600

    Cleaner comments

--- app/src/main/java/com/marlonjones/gyrocheck/MainActivity.java
+++ app/src/main/java/com/marlonjones/gyrocheck/MainActivity.java
@@ -1,6 +1,7 @@
 package com.marlonjones.gyrocheck;
 
 import android.os.Bundle;
+import android.support.v4.*;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -16,6 +17,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        GyroCheck.addTo(this);
+        /**BuildConfig.DEBUG - If the app is in DEBUG mode, it will show the library**/
+
+        if (BuildConfig.DEBUG) {
+            GyroCheck.addTo(this);
+        }
+        else {
+            GyroCheck.addTo(this);
+            /** This is the standard use of the Library. This is
+             just added in this case so the sample can show it in both debug and
+             standard modes. **/
+        }
     }
 }
