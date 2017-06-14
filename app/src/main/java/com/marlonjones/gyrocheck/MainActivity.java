c16a7648574c5906d551740ad7ad1a8347aee38a
Marlon Jones <marlonjones234@gmail.com>
2017-06-13 20:13:33 -0500

    Dialogs and Warnings

--- app/src/main/java/com/marlonjones/gyrocheck/MainActivity.java
+++ app/src/main/java/com/marlonjones/gyrocheck/MainActivity.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.support.v4.*;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.marlonjones.gyrochecklib.GyroCheck;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +19,11 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        new MaterialDialog.Builder(this)
+                .title(R.string.opening_title)
+                .content(R.string.opening)
+                .positiveText(R.string.ok)
+                .show();
 
         /**
          * GyroCheck.addTo(this); - Adds the GyroCheck library to your current view.
@@ -32,5 +40,23 @@ public class MainActivity extends AppCompatActivity {
              * in the view you put it in, no matter the build type.
              **/
         }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_settings) {
+            new MaterialDialog.Builder(this)
+                    .title(R.string.action_settings)
+                    .content(R.string.about_body)
+                    .positiveText(R.string.dismiss)
+                    .show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
